717089c45c89aebb80ce52f4f61a41e0877e7d79
SparkAngel <igor.gnatenko555@gmail.com>
2019-08-02 12:51:05 +0300

    Updates

---
+++ precache-manifest.ca8698bed8b067b16c9c38ddc5ce8463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bc35a0a7e9b268aedb124a6eb19129",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "21f2c9902578827ef951",
+    "url": "/tabs-with-router/static/css/main.fd7fbbd4.chunk.css"
+  },
+  {
+    "revision": "9c513804d50e8d6e33b2",
+    "url": "/tabs-with-router/static/js/2.0f7f7593.chunk.js"
+  },
+  {
+    "revision": "21f2c9902578827ef951",
+    "url": "/tabs-with-router/static/js/main.3e020e97.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
